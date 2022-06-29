a62fa0cf454f29ef27011d64ac5f523b83de89eb
iscoutto <isacoutosoares@gmail.com>
2022-06-29 20:09:19 -0300

    add first file

---
+++ CursoCSharp/Fundamentos/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CursoCSharp
+{
+    class Program {
+        static void Main(string[] args) {
+            var central = new CentralDeExercicios(new Dictionary<string, Action>() {
+                //{"Primeiro Programa - Fundamentos", PrimeiroPrograma.Executar},
+            });
+
+            central.SelecionarEExecutar();
+        }
+    }
+}
