0785cc6e810cce03814caf7bd9fbb7e7525a7636
iscoutto <isacoutosoares@gmail.com>
2022-07-14 19:22:55 -0300

    First commit
    create file

---
+++ CursoCSharp/Fundamentos/PrimeiroPrograma.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace CursoCSharp.Fundamentos
+{
+    public class PrimeiroPrograma
+    {
+        public static void Executar()
+        {
+            Console.WriteLine("Teste executar");
+        }
+    }
+}
+
